56aa7618c46e9ff09d606812af8cfaa9acfb2776
alfariz773 <alfariza338@gmail.com>
2025-10-28 19:59:34 +0530

    day9

--- stdproject/stdapp/views.py
+++ stdproject/stdapp/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 
 def home(request):
-    students = ["james", "jeo", "sam"]
+    students = ["James", "Jeo", "Sam" , "Alfariz", "Rizky", "Dani", "Buji", "Doraemon", "Nobita", "Suneo", "Giant"]
     return render(request, "stdapp/home.html", {'students': students})
 
 
 def res(request, student_name):
     results = {
-        "james": "pass",
-        "jeo": "failed",
-        "sam": "pass"
+        "James": "PASS",
+        "Jeo": "FAILED",
+        "Sam": "PASS",
+        "Alfariz": "PASS",
+        "Rizky": "FAILED",
+        "Dani": "PASS",
+        "Buji": "FAILED",
+        "Doraemon": "PASS",
+        "Nobita": "FAILED",
+        "Suneo": "PASS",
+        "Giant": "FAILED",
+        
     }
-    result = results.get(student_name, "not found")
+    result = results.get(student_name, "no result found")
     return render(request, "stdapp/result.html", {"student_name": student_name, "result": result})
